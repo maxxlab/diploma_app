9cd956eabe289115d564d0d2bf0ffdb01026ab20
Maksym <maxlabyk@gmail.com>
2025-05-05 21:16:04 +0300

    added small changes

--- lib/app/app_bootstrap.dart
+++ lib/app/app_bootstrap.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
+import '../firebase_options.dart';
 import '../services/injector/injector.dart';
 import 'app.dart';
 
 Future<void> bootstrap() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
