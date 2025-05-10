cd477bd34efdece2f9ce19ef7e20f947a8f4a120
Maksym <maxlabyk@gmail.com>
2025-05-10 17:52:10 +0300

    Revert "added models and initial screen"
    This reverts commit 8593cfa596a85dac30c4586a886361c1f00f024c.

--- lib/core/constants/firebase_constants.dart
+++ lib/core/constants/firebase_constants.dart
@@ -1,20 +1,8 @@
-// lib/core/constants/firebase_constants.dart
-
 class FirebaseConstants {
-  // User collections
   static const String usersCollection = 'users';
-  static const String userProfilesCollection = 'user_profiles';
-
-  // Content collections
-  static const String placesCollection = 'places';
-  static const String routesCollection = 'routes';
-  static const String eventsCollection = 'events';
-  static const String reviewsCollection = 'reviews';
-
-  // Social collections
+  static const String postsCollection = 'posts';
   static const String commentsCollection = 'comments';
   static const String likesCollection = 'likes';
 
-  // Constants
   static const int paginationLimit = 20;
 }
--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -4,8 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../screens/auth/login_screen.dart';
 import '../screens/auth/signup_screen.dart';
-import '../screens/map/map_screen.dart';
-import '../screens/profile/profile_screen.dart';
+import '../screens/home/home_screen.dart';
 import '../screens/splash_screen.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
@@ -28,11 +27,7 @@ final appRouter = GoRouter(
     ),
     GoRoute(
       path: '/home',
-      builder: (context, state) => const MapScreen(),
-    ),
-    GoRoute(
-      path: '/profile',
-      builder: (context, state) => const ProfileScreen(),
+      builder: (context, state) => const HomeScreen(),
     ),
   ],
 );
